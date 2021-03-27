0303658624a6dc0e044917a622f9c83b0e20a66b
Butcoin Burning <49112423+wjddnjs33@users.noreply.github.com>
2021-03-27 22:03:19 +0900

    Update upbit.py

--- mini/upbit.py
+++ mini/upbit.py
@@ -1,5 +1,7 @@
 import pyupbit
 import sys
+import time
+from tqdm import tqdm
 
 access_key = 'REDACTED'
 secret_key = 'REDACTED'
@@ -8,40 +10,54 @@ secret_key = 'REDACTED'
 upbit = pyupbit.Upbit(access_key, secret_key)
 ## pyupbit 모듈 연결 확인
 if pyupbit.Upbit:
-    print("[+] Connected")
+    pass
+    #print("[+] Connected")
 else:
     sys.exit()
 
-Name = 'KRW-STORJ'
-Price = 50
-add_money = 15
+Name = 'KRW-AERGO'
+Name_v2 = 'AERGO'
+Price = 434
+number = 22
+add_money = 2
 
+# 첫 매수를 통해 최고점 금액을 미리 정함.
+# 여기서 정해진 first_price 변수가 사용되는 곳은 다음 매수부터 first_price 가격보다 비싸게 사지 않기 위함.
 first_price = 0
 def first_p():
     global first_price
     first_price = pyupbit.get_current_price(Name)
-    print("[+] {} 현재 가격 : {}".format(Name, first_price))
+    #print("[+] {} 현재 가격 : {}".format(Name, first_price))
 
 ## 매수 확인
 def my_money():
     result = 0
-    c = Name.split('-')[1]
     m = upbit.get_balances()
     for i in range(len(m)):
-        if c in m[i]['currency']:
+        if Name_v2 in m[i]['currency']:
             result = 1
             break
         else:
             pass
     return result
 
-# 시장가 매수 
-# name이라는 코인을 price원어치 시장가 매수
-def buy(name, price):
+## 보유 수량 확인
+coin_num = 0
+def check(Na):
+    global coin_num
+    m = upbit.get_balances()
+    for i in range(len(m)):
+        if Na in m[i]['currency']:
+            coin_num = m[i]['balance']
+            break
+        
+def buy(name, price, num):
     result = 0
-    
+    first_p()
     try:
-        ret = upbit.buy_market_order(name, price)
+        ret = upbit.buy_limit_order(name, price, num)
+        print("[+] 매수 가격 : {}".format(price))
+        print("[+] 매수 수량 : {}".format(num))
         if "error" in str(ret):
             e = ret['error']['message']
             print("[+] Error Occur! : {}".format(e))
@@ -54,10 +70,11 @@ def buy(name, price):
 
 # 시장가 매도 
 # name이라는 코인을 num 개수만큼 시장가 매도
-def sell(name, num):
+def sell(name, price, num):
     result = 0
     try:
-        ret = upbit.sell_market_order(name, num)
+        ret = upbit.sell_limit_order(name, price, num)
+        print("[+] 매도 가격 : {}".format(pyupbit.get_current_price(Name)))
         if "error" in str(ret):
             e = ret['error']['message']
             print("[+] Error Occur! : {}".format(e))
@@ -68,25 +85,37 @@ def sell(name, num):
         pass
     return result   
 
-def final_buy():
-    if buy(Name, Price):
+def final_buy(a, b, c):
+    if buy(a, b, c):
         print("[+] Buy Success")
+        print("="*30)
     else:
         print("[+] Buy Failed")
+        print("="*30)
+        sys.exit()
 
 def keep():
     while(1):
         f = pyupbit.get_current_price(Name)
-        print("[+] {} 현재 가격 : {}".format(Name, f))
+        #print("[+] {} 현재 가격 : {}".format(Name, f))
         if my_money():
-            if f > first_price + add_money:
-                print("[+] {} 현재 가격 : {}".format(Name, pyupbit.get_current_price(Name)))
-                if sell(Name, 33):
+            check(Name_v2)
+            '''
+            print("보유 수량 : {}".format(coin_num))
+            print("시작 가격 : {}".format(first_price))
+            print("현재 가격 : {}".format(f))
+            print("더한 가격 : {}".format(first_price + add_money))
+            '''
+            
+            if f >= Price + add_money:
+                #print("[+] {} 현재 가격 : {}".format(Name, pyupbit.get_current_price(Name)))
+                if sell(Name, f, number):
                     print("[+] Sell Success")
+                    print("="*30)
                     while(1): 
-                        if int(pyupbit.get_current_price(Name)) < condition:
-                            first_p()
-                            final_buy()
+                        hang = int(pyupbit.get_current_price(Name))
+                        if hang <= Price:
+                            final_buy(Name, hang, number)
                             break
                         else:
                             pass
@@ -98,10 +127,6 @@ def keep():
             pass
 
 if __name__ == '__main__':
-    # condition 변수에는 특정 코인에 현재 가격이 들어감.
-    # 해당 변수는 매수를 하는데 있어서 해당 가격보다 비싼 가격때에는 사지 않기 하게 위함.
-    condition = pyupbit.get_current_price(Name)
-
-    first_p()
-    final_buy()
+    print("="*30)
+    final_buy(Name, Price, number)
     keep()
