386d373e5ff73dd562473a6da6103d2f63db171e
pocas <pocas.cyber@gmail.com>
2021-07-26 09:15:41 +0900

    Delete I made a challenge/Environment Pollution/Environment Pollution directory

--- I made a challenge/Environment Pollution/Environment Pollution/config/db.sql
+++
@@ -1,8 +0,0 @@
-create database challenge;
-use challenge;
-create table users (
-    name varchar(50) not null,
-    id varchar(50) not null,
-    pw varchar(64) not null
-);
-create table filelist(path varchar(100));
